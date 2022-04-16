7d585337701afc9bde6689c0f562d45d114086c5
mrkouhadi <teacherbryan@outlook.com>
2022-04-17 05:07:25 +0800

    add gesture-handler

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'react-native-gesture-handler';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
