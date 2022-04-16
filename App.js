f526aa2572e5ac83b6355806f9139034dc38d608
mrkouhadi <teacherbryan@outlook.com>
2022-04-17 05:41:48 +0800

    add header right

--- App.js
+++ App.js
@@ -6,6 +6,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'react-native';
 import AddPlace from './src/screens/AddPlace';
 import AllPlaces from './src/screens/AllPlaces';
+import IconBtn from './src/components/ui/IconBtn';
+
 
 const Stack = createNativeStackNavigator();
 
@@ -15,7 +17,9 @@ const App  = () => {
           <StatusBar style='dark' />
           <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen name="AllPlaces"  component={AllPlaces}/>
+                <Stack.Screen name="AllPlaces"  component={AllPlaces} options={{
+                  headerRight:({tintColor})=><IconBtn icon="plus-circle-outline" size={25} color={tintColor} />
+                }}/>
                 <Stack.Screen name="AddPlace"  component={AddPlace}/>
             </Stack.Navigator>
           </NavigationContainer>
