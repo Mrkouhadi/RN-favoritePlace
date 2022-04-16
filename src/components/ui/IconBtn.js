778dd5d71b986309387d3c2463d4b7e6a3336549
mrkouhadi <teacherbryan@outlook.com>
2022-04-17 06:11:24 +0800

    editting the margin of the icon

--- src/components/ui/IconBtn.js
+++ src/components/ui/IconBtn.js
@@ -15,7 +15,6 @@ const IconBtn = ({icon, size, color, onPress }) => {
 const styles = StyleSheet.create({
     button:{
         padding:8,
-        margin:4,
         justifyContent:'center', alignItems:'center'
     },
     pressed:{
