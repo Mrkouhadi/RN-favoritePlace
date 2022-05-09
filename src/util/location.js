9482fc6ea807a23fe70a4b80497d194348db9ae7
mr.kouhadi <teacherbryan@outlook.com>
2022-05-09 20:27:33 +0800

    added imagePreview

--- src/util/location.js
+++ src/util/location.js
@@ -1,5 +1,6 @@
 const GOOGLE_API_KEY = "AIzaSyDoPDktnF7TqlSfBCK5Dyp2ySPH6wuuiD0";
 
-const getMapPreview = ()=>{
-    
+const getMapPreview = (lat, lng)=>{
+    const imagePreview = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=13&size=600x300&maptype=roadmap&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_API_KEY}&signature=YOUR_SIGNATURE`
+    return imagePreview;
 }
