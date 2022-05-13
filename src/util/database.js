6d5144f4ab54ad577cba37e851008475a1fe5945
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2022-05-13 21:20:23 +0800

    add function for data insertion

--- src/util/database.js
+++ src/util/database.js
@@ -1,7 +1,42 @@
 import * as sqlite from 'sqlite'
 
-const database = sqlite.openDatabase('images.db')
+const db = sqlite.openDatabase('images.db')
 
 const init=()=>{
-    
+    const promise = new Promise((resolve, reject)=>{
+        db.transaction(tx =>{
+            tx.executeSql(
+                "CREATE TABLE.......",
+                [],
+                ()=>{
+                    resolve()
+                },
+                (_, error) =>{
+                    reject(error)
+                }
+            )
+        })
+    })
+    return promise;
 }
+
+    // Data insertion
+    const insertUser = (name, age) =>{
+        const promise = new Promise((resolve, reject)=>{
+            db.transaction(tx =>{
+                tx.executeSql(
+                    "INSERT INTO Users (Name, Age) VALUES (?,?)", // u can remove [] and fill in ??
+                    [name, age],
+                    (_, result)=>{
+                        console.log(result);
+                        resolve(result)
+                    },
+                    (_, error) => reject(error)
+                )
+            })
+        })
+        return promise;
+    };
+    const inserUserHanlder = async (name,age) => {  // inside the form component
+        await insertUser(name, age);
+    };
